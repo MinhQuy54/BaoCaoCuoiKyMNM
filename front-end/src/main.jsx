88f5a8dfcbe836aeb0f69c1b236f2f33bacbe66a
th-nhan <tthhaannnnhhaann@gmail.com>
2025-12-13 21:07:34 +0700

    Hoàn thành giao diện trang chủ

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,38 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Import component App
+import './index.css';
 
-// Import các trang mới
-import MovieDetailPage from './page/MovieDetailPage.jsx'
-import VipRegistrationPage from './page/VipRegistrationPage.jsx'
-import RankingPage from './page/RankingPage.jsx'
-import SearchResultPage from './page/SearchResultPage.jsx'
-import MoviePlayerPage from './page/MoviePlayerPage';
+// Chỉ cần import BrowserRouter tại đây để bọc component App
+import { BrowserRouter } from 'react-router-dom'; 
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Trang chủ */}
-        <Route path="/" element={<App />} />
-
-        {/* Trang chi tiết phim */}
-        <Route path="/movie/:id" element={<MovieDetailPage />} />
-
-        {/* Trang Đăng ký VIP */}
-        <Route path="/vip" element={<VipRegistrationPage />} />
-
-        {/* Trang Bảng xếp hạng */}
-        <Route path="/ranking" element={<RankingPage />} />
-
-        {/* Trang Kết quả tìm kiếm */}
-        <Route path="/search" element={<SearchResultPage />} />
-
-        <Route path="/watch/:id" element={<MoviePlayerPage />} />
-        {/* Bạn có thể thêm các route khác như /phimle, /phimbo, /genre/:name tại đây */}
-      </Routes>
+    {/* Chỉ bọc component App bằng BrowserRouter */}
+    <BrowserRouter> 
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
