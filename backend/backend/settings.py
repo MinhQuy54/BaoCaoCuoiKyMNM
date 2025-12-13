939f575d8ceee9c56f67cec2b466da4957de2f14
th-nhan <tthhaannnnhhaann@gmail.com>
2025-12-14 00:20:50 +0700

    chore: remove pycache and add gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'movie_db',
         'USER': 'postgres',
-        'PASSWORD': '542004',
+        'PASSWORD': 'matkhaumoi',
         'HOST': 'localhost',
         'PORT': '5432',
     }
